294c1d99f9bd0dc60fd725e9979ccf628c640ca3
takeuchi-yuya <takei.yuyuyu@mail.com>
2021-01-03 18:42:27 +0900

    修正

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,18 @@
 function header(){
   console.log("ああああ");
-  $.ajax({
-      url: "../include/header.html",
-      cache: false,
-      success: function(html){
-          document.write(html);
-      }
-  });
+  document.write("ブラウザ名：" + navigator.appName + "\n");
+  var html = "";
+  html += '<head>'
+  html += '<link rel="stylesheet" href="../css/master.css" type="text/css">'
+  html += '<div id = "navi">'
+  html += '<a href="#"> info  </a>'
+  html += '<a href="{$root}lab.html"> lab </a>'
+  html +=  ' <a href="{$root}contents.html"> contents </a>'
+  html +=  ' <a href="{$root}blog.html"> blog </a>'
+  html += '</div>'
+  html += '<div id = "logo">'
+  html += '  <a href="{$root}index.html">博物屋</a>'
+  html += '</div>'
+  html += '</head>'
+  document.write(html);
 }
