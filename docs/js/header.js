bdbfbc90cad5fd500db8e3750e7888c810227675
h-den <shiomi.extra@gmail.com>
2021-01-04 23:01:09 +0900

    できねえ

--- docs/js/header.js
+++ docs/js/header.js
@@ -1,11 +1,11 @@
 function header(rootDir){
   $.ajax({
-      url: rootDir + "header.html",  // 読み込むHTMLファイル
+      url: rootDir + "include/header.html",  // 読み込むHTMLファイル
       cache: false,
       async: false,
       dataType: 'html',
       success: function(html){
-          html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
+          html = html.replace(/\{\$root\}/g, rootDir + "html/"); //header.htmlの{$root}を置換
           document.write(html);
       }
   });
