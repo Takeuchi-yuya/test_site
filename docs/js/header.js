345d0caaa31ea83ec7b445e85e2a4de62662a534
h-den <shiomi.extra@gmail.com>
2021-01-05 19:50:08 +0900

    ハンバーガー

--- docs/js/header.js
+++ docs/js/header.js
@@ -10,3 +10,23 @@ function header(rootDir){
       }
   });
 }
+function hamberger(){
+  (function($) {
+    var $nav   = $('#navArea');
+    var $btn   = $('.toggle_btn');
+    var $mask  = $('#mask');
+    var open   = 'open'; // class
+    // menu open close
+    $btn.on( 'click', function() {
+      if ( ! $nav.hasClass( open ) ) {
+        $nav.addClass( open );
+      } else {
+        $nav.removeClass( open );
+      }
+    });
+    // mask close
+    $mask.on('click', function() {
+      $nav.removeClass( open );
+    });
+  } )(jQuery);
+}
